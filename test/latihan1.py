3f7583cc159b67d47c55b1e2a2248eaf369d0f2b
Handi067 <111344381+Handi067@users.noreply.github.com>
2022-11-03 10:12:37 +0700

    Add files via upload

---
+++ test/latihan1.py
@@ -0,0 +1,29 @@
+import time
+start_time = time.time()
+
+print("hello")
+print("world")
+print("halo chantiiek")
+
+# ini adalah comment
+a=10
+print(a)
+
+pesan = "nasi goreng satu!"
+print(pesan)
+
+#penjumlahan
+a=10
+b=50
+print(a+b)
+
+#perkalian
+print(a/b)
+
+print(time.time()- start_time, "detik")
+
+pesan="mie goreng spesial"
+print(pesan)
+
+type("hello world")
+print(type)
